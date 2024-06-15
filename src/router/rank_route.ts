320e54ce6acd015298b4af9b13fc03469929062c
msinwolc <msinwolc@gmail.com>
2024-06-15 15:36:26 +0800

    fix route

--- src/router/rank_route.ts
+++ src/router/rank_route.ts
@@ -4,6 +4,6 @@ import { getRankInfo, getRankInfoByPeriod } from "../controller/rank_controller"
 const router = Router();
 
 router.get("/", getRankInfo);
-router.get("/period:period", getRankInfoByPeriod);
+router.get("/period/:period", getRankInfoByPeriod);
 
 export default router
